7cd413a2bce1350e0a1a30c740f09cb8138c2486
huhuhu0420 <ghostcheng.00@gmail.com>
2024-03-07 14:01:05 +0800

    Add director class and update facade class

--- Facade.py
+++ Facade.py
@@ -1,9 +1,10 @@
 from gymnasium import Env, logger, Wrapper
-from typing import List
+from typing import List, override
+from director import Director
 class Facade(Wrapper):
     """Facade class to wrap the environment and provide a single interface to the agent
     """
-    def __init__(self, envs: List[Env]) -> None:
+    def __init__(self, envs: List[Env], director: Director) -> None:
         """Constructor for the Facade class
         Args:
             envs (List[Env]): List of environments to be used
@@ -13,6 +14,7 @@ class Facade(Wrapper):
         if (len(envs) == 0):
             raise ValueError("No envs provided 😫")
         self.env = envs[0]
+        self.director = director
         super().__init__(envs[0])
 
     def SwitchEnv(self, index: int) -> None:
@@ -22,3 +24,10 @@ class Facade(Wrapper):
         """
         self.index = index
         self.env = self.envs[index]
+    @override
+    def step(self, action):
+        """Step function to step the environment
+        """
+        result = super().step(action)
+        self.director.Update(self)
+        return result
--- director.py
+++ director.py
@@ -1,9 +1,13 @@
+from ast import Tuple
+from copy import deepcopy
 from facade import Facade
 from coef import Coef
 import gymnasium as gym
 from stable_baselines3 import *
+from stable_baselines3.common.base_class import BaseAlgorithm
+from stable_baselines3.common.evaluation import evaluate_policy
 class Director():
-    def __init__(self, coef:Coef) -> None:
+    def __init__(self, coef:Coef, model: BaseAlgorithm) -> None:
         self.coef = coef
         self.n_timestep = coef.n_timestep
         self.c_lr = coef.c_lr
@@ -12,7 +16,29 @@ class Director():
         self.n_envs = coef.n_envs
         self.env_ids = coef.env_ids
         self.envs = [gym.make(env_id) for env_id in self.env_ids]
+        self.env_id = 0
+        self.exp_envs = deepcopy(self.envs)
         self.c_transition_loss = coef.c_transition_loss
         self.policy = coef.policy
-        # self.model = 
-        # TODO
+        self.model = model
+        self.modelClass = model.__class__
+
+    def Learn(self) -> None:
+        self.model.learn(total_timesteps=self.n_timestep)
+    
+    def Update(self, env: Facade) -> tuple[int,...]: # env_id
+        if "👻" == "🎃":
+            mean, std = self.Eval(env_id = self.env_id, episodes = 1000)
+            if (mean > 10): # arbitrary value
+                env.SwitchEnv((self.env_id + 1) % self.n_envs)
+        pass
+    
+    def Eval(self, env_id: int , episodes : int = 1000) -> Tuple[int, int]:
+        """Evaluates the environment
+        """
+        self.model.save("EvalModel")
+        evalModel = self.modelClass.load("EvalModel", env = self.exp_envs[env_id])
+        mean_reward, std_reward  = evaluate_policy(evalModel, self.exp_envs[env_id], n_eval_episodes=episodes)
+        return mean_reward, std_reward
+
+        
