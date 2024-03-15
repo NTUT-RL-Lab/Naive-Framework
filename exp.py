21223fdade9e4e624e7df51b17f716b2a8177118
huhuhu0420 <ghostcheng.00@gmail.com>
2024-03-15 10:03:55 +0800

    🎂🌲

--- exp.py
+++ exp.py
@@ -1,6 +1,8 @@
 import gymnasium as gym
 from director import Director
 from gymnasium import logger
+from gymnasium.wrappers.pixel_observation import PixelObservationWrapper
+from gymnasium.wrappers.resize_observation import ResizeObservation
 from coef import Coef
 from facade import Facade
 from stable_baselines3 import *
@@ -28,13 +30,31 @@ def main():
     logger.set_level(logger.INFO)
     logger.info("👻")
     director = Director(coef)
-    envs = [gym.make(env_id) for env_id in coef.env_ids]
+    envs = birth_envs(coef.env_ids)
     facade = Facade(envs, director=director)
     model = PPO(coef.policy, facade)
     director.set_model(model)
 
     director.learn()
     print("Learning done")
+
+
+def birth_envs(env_ids) -> list[gym.Env]:
+    """Births the environments
+    """
+    max_w, max_h = 0, 0
+    envs = [gym.make(env_id, render_mode="rgb_array") for env_id in env_ids]
+    for env in envs:
+        env = PixelObservationWrapper(env)
+        obs, _ = env.reset()
+        max_w = max(max_w, obs['pixels'].shape[0])
+        max_h = max(max_h, obs['pixels'].shape[1])
+
+        env.observation_space = obs['pixels']
+        print(env.observation_space.shape)
+    for env in envs:
+        env = ResizeObservation(env, (max_w, max_h))
+    return envs
 
 
 if __name__ == '__main__':
