e19d8fa2536e44a0090265bce0d656e823eba378
huhuhu0420 <ghostcheng.00@gmail.com>
2024-08-16 21:03:29 +0800

    🌈

---
+++ config/die_rainbow_sp.py
@@ -0,0 +1,66 @@
+from copy import deepcopy
+from easydict import EasyDict
+from not_die_alone import not_alone
+from not_die_eval import not_die_eval
+spaceinvaders_rainbow_config = dict(
+    exp_name='spaceinvaders_rainbow_seed0',
+    env=dict(
+        collector_env_num=1,
+        evaluator_env_num=1,
+        n_evaluator_episode=8,
+        stop_value=10000000,
+        manager=dict(shared_memory=False, )
+    ),
+    policy=dict(
+        cuda=True,
+        priority=False,
+        priority_IS_weight=False,
+        load_path='spaceinvaders_rainbow_seed0_240810_100023/ckpt/ckpt_best.pth.tar',
+        model=dict(
+            obs_shape=[1, 84, 84],
+            action_shape=6,
+            encoder_hidden_size_list=[128, 128, 512],
+            v_min=-10,
+            v_max=10,
+            n_atom=51,
+        ),
+        nstep=3,
+        discount_factor=0.99,
+        learn=dict(
+            update_per_collect=10,
+            batch_size=32,
+            learning_rate=0.0001,
+            target_update_freq=500,
+            iqn=False,
+        ),
+        collect=dict(n_sample=100, ),
+        eval=dict(evaluator=dict(eval_freq=4000, )),
+        other=dict(
+            eps=dict(
+                type='exp',
+                start=0.05,
+                end=0.05,
+                decay=1000000,
+            ),
+            replay_buffer=dict(replay_buffer_size=400000, ),
+        ),
+    ),
+)
+spaceinvaders_rainbow_config = EasyDict(spaceinvaders_rainbow_config)
+main_config = spaceinvaders_rainbow_config
+spaceinvaders_rainbow_create_config = dict(
+    env_manager=dict(type='subprocess'),
+    policy=dict(type='rainbow'),
+)
+spaceinvaders_rainbow_create_config = EasyDict(
+    spaceinvaders_rainbow_create_config)
+create_config = spaceinvaders_rainbow_create_config
+
+
+def main():
+    not_alone((main_config, create_config), seed=0, max_env_step=30000000)
+
+
+def eval():
+    not_die_eval(main_config, create_config,
+                 'spaceinvaders_rainbow_seed0_240810_100023/ckpt/ckpt_best.pth.tar')
